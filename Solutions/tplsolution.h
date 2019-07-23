92bb0124eab3c35d8b823405941cb6a8e2f1cb62
alfredwei <alfred.wei@orbweb.com>
2019-07-23 16:40:39 +0800

    Init VS Code with Cpp on Mac

--- Solutions/tplsolution.h
+++ Solutions/tplsolution.h
@@ -3,14 +3,18 @@
 
 namespace TplSolution {
     using namespace std;
+    
     template < typename T, size_t N >
     constexpr size_t arraySize ( T (&)[N] ) noexcept {
         return N;
     }
 
+    template< typename T >
+    class TypeResolveViewer;
+
     void test_tpl_solution()
     {
         int arr[] = {1,2,3,4,6};
-        std::array<int, arraySize(arr)> val;
+        array<int, arraySize(arr)> val;
     }
 }
